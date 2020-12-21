0fd326d9c85a0f87ff234fbe44f8b09f69fc90af
Elena <lenhenlady@gmail.com>
2020-12-21 17:15:44 +0300

    additional folders and files deleted

--- helpers/files.js
+++
@@ -1,7 +0,0 @@
-const fsPromises = require('fs').promises;
-
-const getFileData = (pathToFile) => fsPromises.readFile(pathToFile, { encoding: 'utf-8' })
-  .then((data) => JSON.parse(data))
-  .catch((error) => console.error(error));
-
-module.exports = getFileData;
